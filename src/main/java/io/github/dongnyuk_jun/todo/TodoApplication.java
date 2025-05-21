9a36624610fb1ff102176fdf2f971fefa893dcba
전동녘 <dongnyuk@dongnyuks-MacBook-Air.local>
2025-05-21 15:40:03 +0900

    Initial commit

---
+++ src/main/java/io/github/dongnyuk_jun/todo/TodoApplication.java
@@ -0,0 +1,15 @@
+package io.github.dongnyuk_jun.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args)
+	{
+		System.out.println(23);
+		//SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dongnyuk_jun/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dongnyuk_jun.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
